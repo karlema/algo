469f77b2c52bd42f2c3fdd806004789705c9c8e9
karlema <30691193+karlema@users.noreply.github.com>
2017-09-25 00:13:30 +0900

    Add files via upload

--- nhn.cpp
+++ nhn.cpp
@@ -3,6 +3,7 @@
 #include<algorithm>
 #include<string>
 using namespace std;
+char result[30];
 void one_left_rotate(char arr[],int end)
 {
 	
@@ -24,25 +25,69 @@ void many_left_rotate(char arr[], int end,int k)
 
 	printf("여러번 왼쪽 로테이트\n");
 	char temp[30] = { 0, };
-	for (int i = 0; i < strlen(temp); i++)
+	for (int i = 0; i < end; i++)
 	{
 		temp[i] = arr[i];
 	}
-	for (int i = 0; i < end - 1; i++)
+	for (int i = 0; i < end; i++)
+	{
+		printf("%c", temp[i]);
+	}
+	printf("3개쉬프트\n");
+	for (int i = 0; i < end - k; i++)
 	{
 		arr[i] = arr[i + k];
 	}
-	for (int i = k-1; i < end - 1; i++)
+	for (int i = 0; i < end; i++)
 	{
-
+		printf("%c", arr[i]);
 	}
-	printf("%d", end);
-	for (int i = 0; i < end - 1; i++)
+	printf("결과값\n");
+	for (int i = end - k; i <end ; i++)
 	{
-		arr[i] = arr[i + 1];
+		arr[i] = temp[i-end+k];
+	}
+
+	//printf("%d", end);
+	for (int i = 0; i < end; i++)
+	{
+		result[i] = arr[i];
+		printf("%c", arr[i]);
+	}
+}
+void many_right_rotate(char arr[], int end, int k)
+{
+
+	printf("여러번 오른쪽 로테이트\n");
+	char temp[30] = { 0, };
+	for (int i = 0; i < end; i++)
+	{
+		temp[i] = arr[i];
 	}
 	for (int i = 0; i < end; i++)
 	{
+		printf("%c", temp[i]);
+	}
+	printf("3개쉬프트\n");
+	for (int i =0; i < end-k; i++)
+	{
+		arr[i+k] = temp[i];
+		printf("%d %d\n",i+k,i);
+	}
+	for (int i = 0; i < end; i++)
+	{
+		printf("%c", arr[i]);
+	}
+	printf("결과값\n");
+	for (int i = 0; i <k; i++)
+	{
+		arr[i] = temp[end - k+i];
+	}
+
+	//printf("%d", end);
+	for (int i = 0; i < end; i++)
+	{
+		result[i] = arr[i];
 		printf("%c", arr[i]);
 	}
 }
@@ -66,16 +111,15 @@ int main()
 		//
 		for (int i = 0; i < strlen(secret); i++)
 		{
-			secal[i] = secret[i] - 97;
+			secal[i] = (secret[i] - 'a' + message[i] - 'a') % 26;
 			printf("%d ", secal[i]);
 		}
 
 		printf("\n메세지\n");
 		for (int i = 0; i < strlen(secret); i++)
 		{
-			
 			printf("%d ", message[i]);
-			encal[i] = secal[i] + message[i];
+			encal[i] = (secal[i])+'a';
 		}
 		printf("\n마지막엔크립션\n");
 		for (int i = 0; i < strlen(secret); i++)
@@ -83,12 +127,52 @@ int main()
 			
 			printf("%c ", encal[i]);
 		}
-	
+		if (rotate > 0)
+		{
+			many_left_rotate(encal, strlen(secret), rotate);
+		}
+		else
+		{
+			many_right_rotate(encal, strlen(secret), rotate);
+		}
 	}
 	
 	else if (!strcmp(iden, "decrypt"))
 	{
 		printf("디크립션");
+		if (rotate > 0)
+		{
+			many_right_rotate(message, strlen(message), rotate);
+		}
+		else
+		{
+			many_left_rotate(message, strlen(message), rotate);
+		}
+		printf("시크릿코드\n");
+		//
+		for (int i = 0; i < strlen(secret); i++)
+		{
+			secal[i] = ( (result[i] - 'a')- (secret[i] - 'a') ) % 26;
+			printf("%d ", secal[i]);
+			if (secal[i] < 0)
+			{
+				secal[i] = secal[i] + 26;
+			}
+		}
+
+		printf("\n메세지\n");
+		for (int i = 0; i < strlen(secret); i++)
+		{
+			printf("%d ", result[i]);
+			encal[i] = (secal[i]) + 'a';
+		}
+		printf("\n마지막엔크립션\n");
+		for (int i = 0; i < strlen(secret); i++)
+		{
+
+			printf("%c ", encal[i]);
+		}
+		
 	}
 	else
 	{
